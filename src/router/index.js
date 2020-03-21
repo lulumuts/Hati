e34e051fd68c5d0f403e2cb5d63b02948bd41c68
lulumuts <lulumutuli24@gmail.com>
2020-03-22 01:52:34 +0300

    authentication flow set up

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import auth from '@/auth'
+import Dashboard from '@/components/Dashboard.vue'
+import Login from '@/components/Login.vue'
 import { CardPlugin, LayoutPlugin, FormPlugin, FormInputPlugin } from 'bootstrap-vue'
 
 Vue.use(Router)
@@ -10,11 +13,39 @@ Vue.use(FormPlugin)
 Vue.use(FormInputPlugin)
 
 export default new Router({
+  mode: 'history',
+  base: __dirname,
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/dashboard',
+      component: Dashboard,
+      beforeEnter: requireAuth
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/logout',
+      beforeEnter (to, from, next) {
+        auth.logout()
+        next('/')
+      }
     }
   ]
 })
+function requireAuth (to, from, next) {
+  if (!auth.loggedIn()) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+}
